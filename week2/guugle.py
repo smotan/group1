d1d47b6261132d00b854d64f11d8772125c330bb
Anna Smoter <an.smoter@gmail.com>
2019-11-06 14:10:09 +0100

    kopioitu koodi

--- week2/guugle.py
+++ week2/guugle.py
@@ -1,1 +1,10 @@
  #Hakukone
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(lowercase=True, binary=True)
+sparse_matrix = cv.fit_transform(texthere)
+td_matrix = sparse_matrix.todense().T
+
+t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
+print("Query: example")
+print(td_matrix[t2i["example"]])
