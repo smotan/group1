63a38915689010134986637497cebbc2c4cf6570
Varpu Vehomaki <pauhula.vv@hotmail.com>
2019-11-13 19:27:19 +0200

    Exact match works, started to work on stemming

--- week3/guugle.py
+++ week3/guugle.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+import re
    
 def read_from_file():   
     try:
@@ -24,25 +25,32 @@ def main():
         print("Quit: q & enter")
         query = input("Query: ")
         query = query.lower()
+	quotes = re.search("^\".+\"$", query)
+	list_of_articles = read_from_file()	
 
-        if query == "q":
+	if(quotes):
+	    no_parse(query, list_of_articles)	    
+
+        elif query == "q":
             print("Goodbye!")
             break
-        list_of_articles = read_from_file()
         
-        
-        tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
-        sparse_matrix = tfv.fit_transform(list_of_articles).T.tocsr()
-        query_vec = tfv.transform([query]).tocsc()        
-        hits = np.dot(query_vec, sparse_matrix)
-        ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
-        articles = 0
-        for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
-            article = list_of_articles[doc_idx]
-            doc = article[article.find(query)-100:article.find(query)+100]
-            if score >= 0.02:
-                articles += 1
-                print("Score: {:.4f}: {:s}".format(score, doc))
-        print("Found", articles, "articles")
-        
+def no_parse(query, list_of_articles):
+	query = re.sub("\"", '', query)
+	try:        
+            tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+            sparse_matrix = tfv.fit_transform(list_of_articles).T.tocsr()
+            query_vec = tfv.transform([query]).tocsc()        
+            hits = np.dot(query_vec, sparse_matrix)
+            ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
+            articles = 0
+            for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
+                article = list_of_articles[doc_idx]
+                doc = article[article.find(query)-100:article.find(query)+100]
+                if score >= 0.02:
+                    articles += 1
+                    print("Score: {:.4f}: {:s}".format(score, doc))
+            print("Found", articles, "articles")
+        except IndexError:
+	    print("No results")
 main()
