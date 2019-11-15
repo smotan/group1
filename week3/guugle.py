6919846ca0f0bf095feb18ac02b6f51419f3e91e
Varpu Vehomaki <pauhula.vv@hotmail.com>
2019-11-15 12:07:44 +0200

    Stemming and exact match should maybe work

--- week3/guugle.py
+++ week3/guugle.py
@@ -1,6 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import re
+from nltk.stem import LancasterStemmer
    
 def read_from_file():   
     try:
@@ -34,7 +35,36 @@ def main():
         elif query == "q":
             print("Goodbye!")
             break
-        
+        else:
+            parse(query, list_of_articles)
+
+def parse(query, list_of_articles):
+    porter = LancasterStemmer()
+    query = porter.stem(query)
+    stem_list_of_articles = []
+    results2 = [line.split() for line in list_of_articles]
+    results2 = [ x for x in results2 if x != []]
+    for i in range(0, len(results2)):
+         l1 = results2[i]
+         l2 = ' '.join([porter.stem(word) for word in l1])
+         stem_list_of_articles.append(l2)        
+    try:        
+        tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+        sparse_matrix = tfv.fit_transform(stem_list_of_articles).T.tocsr()
+        query_vec = tfv.transform([query]).tocsc()        
+        hits = np.dot(query_vec, sparse_matrix)
+        ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
+        articles = 0
+        for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
+            article = list_of_articles[doc_idx]
+            doc = article[article.find(query)-100:article.find(query)+100]
+            if score >= 0.02:
+                articles += 1
+                print("Score: {:.4f}: {:s}".format(score, doc))
+        print("Found", articles, "articles")
+    except IndexError:
+        print("No results")            
+
 def no_parse(query, list_of_articles):
 	query = re.sub("\"", ' ', query)
 	try:        
