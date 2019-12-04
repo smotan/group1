fe45dd21eec1a66a31802191918b096022d43a80
Vehomäki Varpu E A <vaveva@melkki.cs.helsinki.fi>
2019-12-04 19:43:47 +0200

    Some changes

--- final_project/themes.py
+++ final_project/themes.py
@@ -4,20 +4,26 @@ def read_from_file():
     try:
         text_file = open("songs.txt", "r")
         file_text = text_file.read()
-        list_of_songs = file_text.split("</article>")
+        list_of_songs = file_text.split("Title: ")
         return list_of_songs
     except OSError:
         print("File not found")
 
 def main():
     songs = read_from_file()
-    songs = songs[:10]
-    for song in songs:
-         extractor = pke.unsupervised.TopicRank()
-         extractor.load_document(input=songs, language='en')
-         extractor.candidate_selection()
-         extractor.candidate_weighting()
-         keyphrases = extractor.get_n_best(n=10)
-         print(song)
-         for keyphrase in keyphrases:
-             print(keyphrase[0] + ", score=" + str(keyphrase[1]))
+    index = 1
+    songs_and_themes = []
+    while index <= len(songs):
+        try:
+            song = songs[index]
+            extractor = pke.unsupervised.TopicRank()
+            extractor.load_document(input=song, language='en')
+            extractor.candidate_selection()
+            extractor.candidate_weighting()
+            keyphrases = extractor.get_n_best(n=10)
+            index += 1
+        except ValueError:
+            print("No")
+            pass
+
+main()
