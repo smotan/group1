9cca96e3c0253702b33595d21b802ebae493f149
Anna Smoter <an.smoter@gmail.com>
2020-01-05 20:29:59 +0100

    some changes

--- final_project/ui.py
+++ final_project/ui.py
@@ -22,17 +22,21 @@ try:
     list_of_songs = list(filter(None, list_of_songs))
     theme_file = open("themes1.txt", "r")
     read_themes = theme_file.read()
+
+    # themes is a list of strings in the format (theme, score) - up to ten themes for each song, seperated with ", "
     themes = read_themes.split("\n")
     list_of_themes = []
+
+    # making a list of lists of tuples from a list of strings
     for song_themes in themes:
         list_of_themes.append(str2tuplelist(song_themes))
-    #WE WANT to have list of songs as list of triples (author, title, song)!!!!!!!!!!!!!!
 
 except OSError:
     print("File not found")
 
-
-def search_more_words(query):
+# searching more words as a theme - if a search contains more than one word, 
+# firstly we search for them as one theme, then seperately as mor than one theme
+def search_more_words_as_a_theme(query):
     list_of_idx = []
     for i in range(len(list_of_themes)):
         song_themes_and_scores = list_of_themes[i]
@@ -42,6 +46,7 @@ def search_more_words(query):
                     list_of_idx.append(i)
     return list_of_idx
 
+# literal search
 def literal_search(query):
     list_of_idx = []
     for i in range(len(list_of_songs)):
@@ -49,8 +54,9 @@ def literal_search(query):
             list_of_idx.append(i)
     return list_of_idx
 
-def search_query(query):
 
+# searching for a query consisting of one or more themes
+def search_theme(query):
     query = query.split()
     dict_of_matches = {}
 
@@ -68,63 +74,60 @@ def search_query(query):
     list_of_matches = sorted(dict_of_matches, key=dict_of_matches.get, reverse=True)
     return list_of_matches
 
-#Gets random themes to show on the website
-def find_random(list_of_themes):
-    list_of_themes = str(list_of_themes)
-    remove_digits = str.maketrans('', '', digits)
-    res = list_of_themes.translate(remove_digits)
-    res = re.sub("\[", "", res)
-    res = re.sub("\(\'", "", res)
-    res = re.sub("\'\,", ",", res)
-    res = re.sub("\.\)", "", res)
-    res = re.sub(" ,", "", res)
-    res = re.sub("]", "", res)
-    list_of_themes = res.split(",")
-    num_to_select = 10                           # set the number to select here.
-    list_of_random_items = random.sample(list_of_themes, num_to_select)
+# gets random themes to show on the website
+def find_random():
+    all_themes = []
+    for song_themes_and_scores in list_of_themes:
+        for theme in song_themes_and_scores:
+            all_themes.append(theme[0])
+    num_to_select = 10  # set the number to select here.
+    list_of_random_items = random.sample(all_themes, num_to_select)
+    
     return(list_of_random_items)
 
-def rewrite_token(t, td_matrix, t2i):
-    d = {"and": "&", "AND": "&",
-     "or": "|", "OR": "|",
-     "not": "1 -", "NOT": "1 -",
-     "(": "(", ")": ")"}
-    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) 
+#def rewrite_token(t, td_matrix, t2i):
+ #   d = {"and": "&", "AND": "&",
+  #   "or": "|", "OR": "|",
+   #  "not": "1 -", "NOT": "1 -",
+    # "(": "(", ")": ")"}
+    #return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) 
 
-
-def rewrite_query(themes_query, td_matrix, t2i): # rewrite every token in the query
-    return " ".join(rewrite_token(t, td_matrix, t2i) for t in themes_query.split())
+# not theme ei toimi
+# PS mulla puhelin meni rikki ja ei oo nyt whatsappia, voi kommentoida tässä !!!
+#def rewrite_query(themes_query, td_matrix, t2i): # rewrite every token in the query
+#    return " ".join(rewrite_token(t, td_matrix, t2i) for t in themes_query.split())
 
 #Searches for matches in the list of themes
-def search_themes(themes_query, list_of_themes, list_of_songs):
-    list_of_art = []
+#def search_themes(themes_query, list_of_themes, list_of_songs):
+#    list_of_art = []
 
-    try:
-        cv = CountVectorizer(lowercase=True, binary=True)
-        sparse_matrix = cv.fit_transform(themes)
-        td_matrix = sparse_matrix.todense().T
-        t2i = cv.vocabulary_
+#    try:
+#        cv = CountVectorizer(lowercase=True, binary=True)
+#        sparse_matrix = cv.fit_transform(themes)
+#        td_matrix = sparse_matrix.todense().T
+#        t2i = cv.vocabulary_
 
-        hits_matrix = eval(rewrite_query(themes_query, td_matrix, t2i))
-        hits_list = list(hits_matrix.nonzero()[1])
-        for doc_idx in hits_list:
-            doc = list_of_songs[doc_idx]
-            title = re.sub('.*Title:.(.*).Lyrics.*', r'\1', doc[:100], flags=re.S)
-            author = re.sub('(.*).Title.*', r'\1', doc[:100], flags=re.S)
-            text = re.sub(r'.*Lyrics:.(.*)', r'\1', doc, flags=re.S)
-            text = text.replace('\n', '<br>')
-            teemat = ""
-            for theme in list_of_themes[doc_idx]:
-                teemat += " "
-                teemat += str(theme[0])
-            list_of_art.append({'author':author, 'title':title,'sisalto':text, 'themes':teemat})
+#        hits_matrix = eval(rewrite_query(themes_query, td_matrix, t2i))
+#        hits_list = list(hits_matrix.nonzero()[1])
+#        for doc_idx in hits_list:
+#            doc = list_of_songs[doc_idx]
+#            title = re.sub('.*Title:.(.*).Lyrics.*', r'\1', doc[:100], flags=re.S)
+#            author = re.sub('(.*).Title.*', r'\1', doc[:100], flags=re.S)
+#            text = re.sub(r'.*Lyrics:.(.*)', r'\1', doc, flags=re.S)
+#            text = text.replace('\n', '<br>')
+#            teemat = ""
+#            for theme in list_of_themes[doc_idx]:
+                #teemat += " "
+                #teemat += str(theme[0])
+#                teemat = " ".join(str(theme[0]))
+#            list_of_art.append({'author':author, 'title':title,'sisalto':text, 'themes':teemat})
 
-        return list_of_art
-    except ValueError:
-        pass
-    except KeyError:
-        matches = []
-        return matches
+#        return list_of_art
+#    except ValueError:
+#        pass
+#    except KeyError:
+#        matches = []
+#        return matches
 
 #Function search() is associated with the address base URL + "/search"
 @app.route('/search')
@@ -134,7 +137,7 @@ def search():
     query = request.args.get('query')
     themes_query = request.args.get('themes_query')
     random_themes = []
-    random_themes = find_random(list_of_themes)
+    random_themes = find_random()
     #Initialize list of matches
     boolean_matches = []
     matches = []
@@ -147,15 +150,6 @@ def search():
         #else:
         query = query.lower()
         list_of_matches = literal_search(query)
-        if len(query.split()) > 1:
-            list_idx = search_more_words(query)
-            for idx in list_idx:
-                if idx not in list_of_matches:
-                    list_of_matches.append(idx)
-        list_idx = search_query(query)
-        for idx in list_idx:
-                if idx not in list_of_matches:
-                    list_of_matches.append(idx)
      
         for idx in list_of_matches:
             doc = list_of_songs[idx]
@@ -164,14 +158,27 @@ def search():
             text = re.sub(r'.*Lyrics:.(.*)', r'\1', doc, flags=re.S)
             text = text.replace('\n', '<br>')
             themes = ""
-            for theme in list_of_themes[idx]:
-                themes += " "
-                themes += str(theme[0])
+            for themes_and_scores in list_of_themes[idx]:
+                themes += themes_and_scores[0] + " "
             matches.append({'author':author, 'title':title,'sisalto':text, 'themes':themes})
 
     #If user searches for matches in the list of themes
     elif themes_query:
-        matches = search_themes(themes_query, list_of_themes, list_of_songs)
+        list_of_matches = search_more_words_as_a_theme(themes_query)
+        for result in search_theme(themes_query):
+            if result not in list_of_matches:
+                list_of_matches.append(result)
+        for idx in list_of_matches:
+            doc = list_of_songs[idx]
+            title = re.sub('.*Title:.(.*).Lyrics.*', r'\1', doc[:100], flags=re.S)
+            author = re.sub('(.*).Title.*', r'\1', doc[:100], flags=re.S)
+            text = re.sub(r'.*Lyrics:.(.*)', r'\1', doc, flags=re.S)
+            text = text.replace('\n', '<br>')
+            themes = ""
+            for themes_and_scores in list_of_themes[idx]:
+                themes += themes_and_scores[0] + " "
+            matches.append({'author':author, 'title':title,'sisalto':text, 'themes':themes})
+
 
     #Render index.html with matches variable
     return render_template('index.html', matches=matches, random_themes=random_themes)
